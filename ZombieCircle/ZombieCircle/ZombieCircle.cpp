aa755fbc9247272cdcc0da18366c7c9554b14ece
AntoineEsbelin <74203837+AntoineEsbelin@users.noreply.github.com>
2021-11-08 22:41:47 +0100

    Player
    Player

--- ZombieCircle/ZombieCircle/ZombieCircle.cpp
+++ ZombieCircle/ZombieCircle/ZombieCircle.cpp
@@ -1,37 +1,61 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
 #include "windows.h"
 #include <iostream>
 #include "Header.h"
+#include <vector>
 
-
-
+using namespace sf;
+using namespace std;
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(1600, 1200), "Ma première fenêtre");
+	// Fenêtre
+	RenderWindow window(VideoMode(1100, 1100), "Ma première fenêtre");
 	window.setVerticalSyncEnabled(true);
 
-	sf::RectangleShape rect(sf::Vector2f(100, 100));
-	rect.setPosition(sf::Vector2f(400.f, 300.f));
+	// Player
 
-	sf::RectangleShape bg(sf::Vector2f(1200, 1200));
-	sf::FloatRect bgRect = bg.getLocalBounds();
+	RectangleShape player(Vector2f(50, 50));
+	player.setPosition(Vector2f(400.f, 300.f));
+
+	// Munitions
+	CircleShape projectile;
+	projectile.setFillColor(Color::Red);
+	projectile.setRadius(5.f);
+
+	vector<CircleShape> projectiles;
+	projectiles.push_back(CircleShape(projectile));
+	// Enemies
+	RectangleShape enemy;
+	enemy.setFillColor(Color::Magenta);
+	enemy.setSize(Vector2f(50.f, 50.f));
+	enemy.setPosition(window.getView().getCenter());
+	
+	vector <RectangleShape> enemies;
+	//enemies.push_back(RectangleShape(enemy));
+
+	// Background
+
+	RectangleShape bg(Vector2f(1100, 1100));
+	FloatRect bgRect = bg.getLocalBounds();
 	bg.setOrigin(bgRect.left + bgRect.width / 2.0f, bgRect.top + bgRect.height / 2.0f);
 	bg.setPosition(window.getView().getCenter());
-	bg.setFillColor(sf::Color::Green);
+	bg.setFillColor(Color::Black);
 	
 
-	sf::Clock clock;
+	Clock clock;
 
 	while (window.isOpen())
 	{
 		// Inputs
-		sf::Event event;
+		Event event;
 		while (window.pollEvent(event))
 		{
 			switch (event.type)
 			{
-				case sf::Event::Closed:
+				case Event::Closed:
 					window.close();
 					break;
 
@@ -41,16 +65,24 @@ int main()
 		}
 
 		// Logique
-		sf::Time elapsedTime = clock.restart(); //< Calcul du temps écoulé depuis la dernière boucle
+		Time elapsedTime = clock.restart(); //< Calcul du temps écoulé depuis la dernière boucle
 
-		RectMovement(rect, elapsedTime.asSeconds());
+		RectMovement(player, elapsedTime.asSeconds());
 
 		// Rendu
 		window.clear();
 
 		
 		window.draw(bg);
-		window.draw(rect);
+		window.draw(player);
+		window.draw(enemy);
+
+		for (size_t i = 0; i = enemies.size(); i++) 
+			window.draw(enemies[i]);
+		
+
+		for (size_t i = 0; i < projectiles.size(); i++)
+			window.draw(projectiles[i]);
 
 		window.display();
 	}
@@ -61,17 +93,17 @@ void RectMovement(sf::RectangleShape& rect, float deltaTime)
 {
 	float speed = 300.f; // 300 pixels par seconde
 
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-		rect.move(sf::Vector2f(0.f, -speed * deltaTime));
+	if (Keyboard::isKeyPressed(Keyboard::Up))
+		rect.move(Vector2f(0.f, -speed * deltaTime));
 
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-		rect.move(sf::Vector2f(0.f, speed * deltaTime));
+	if (Keyboard::isKeyPressed(Keyboard::Down))
+		rect.move(Vector2f(0.f, speed * deltaTime));
 
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-		rect.move(sf::Vector2f(-speed * deltaTime, 0.f));
+	if (Keyboard::isKeyPressed(Keyboard::Left))
+		rect.move(Vector2f(-speed * deltaTime, 0.f));
 
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-		rect.move(sf::Vector2f(speed * deltaTime, 0.f));
+	if (Keyboard::isKeyPressed(Keyboard::Right))
+		rect.move(Vector2f(speed * deltaTime, 0.f));
 }
 
 
