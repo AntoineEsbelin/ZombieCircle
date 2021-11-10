4d6c1ad62169931a96a57dc6340ea53819e58970
Romain <mromain911@gmail.com>
2021-11-10 17:55:44 +0100

    header

--- ZombieCircle/ZombieCircle/Header.h
+++ ZombieCircle/ZombieCircle/Header.h
@@ -1,3 +1,19 @@
 #pragma once
 
-void RectMovement(sf::RectangleShape& rect, float deltaTime);
+//void RectMovement(sf::RectangleShape& rect, float deltaTime);
+
+using namespace sf;
+using namespace std;
+
+class Bullet {
+public:
+	CircleShape shape;
+	Vector2f currVelocity;
+	float maxSpeed;
+
+	Bullet(float radius = 5.f) : currVelocity(0.f, 0.f), maxSpeed(15.f)
+	{
+		this->shape.setRadius(radius);
+		this->shape.setFillColor(Color::Red);
+	}
+};
